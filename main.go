bc29c40c50fa0caaa61e5a03600aa005e48d138f
klothe <klothe@users.noreply.github.com>
2016-01-22 00:04:49 -0500

    add main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"matasano-crypto-challenge-solutions-go/set1"
+)
+
+func main() {
+	set1.Challenge1()
+	set1.Challenge2()
+	set1.Challenge3()
+	set1.Challenge4()
+	set1.Challenge5()
+	set1.Challenge6()
+	set1.Challenge7()
+	set1.Challenge8()
+}
